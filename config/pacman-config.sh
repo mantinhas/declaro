1c39ebd7c6621cc24745e6ebee71c21c5befb311
mantinhas <mantinhas@github.com>
2025-04-13 16:47:42 +0100

    add better comments and template config

--- config/pacman-config.sh
+++ config/pacman-config.sh
@@ -1,5 +1,8 @@
 KEEPLISTFILE="/etc/declaro/packages.list"
 
+# Command to install a package and its dependencies (no confirm/user prompts)
 UNINSTALL_COMMAND="sudo pacman -Rns --noconfirm"
+# Command to install a package and its dependencies (no confirm/user prompts)
 INSTALL_COMMAND="sudo pacman -S --noconfirm"
+# Command to list all manually/explicitely installed packages
 LIST_COMMAND="pacman -Qqe"
---
+++ config/template-config.sh
@@ -0,0 +1,8 @@
+KEEPLISTFILE="/etc/declaro/packages.list"
+
+# Command to uninstall package and its dependencies (no confirm/user prompts)
+UNINSTALL_COMMAND=""
+# Command to install a package and its dependencies (no confirm/user prompts)
+INSTALL_COMMAND=""
+# Command to list all manually/explicitely installed packages
+LIST_COMMAND=""
