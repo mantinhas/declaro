a62ee669e3fa79d5d1c88c82a42bea7f392566a4
mantinhas <mantinhas@github.com>
2025-05-11 15:37:33 +0100

    apt debian/ubuntu config

---
+++ config/apt-config.sh
@@ -0,0 +1,14 @@
+KEEPLISTFILE="/etc/declaro/packages.list"
+
+# Command to install a package and its dependencies (no confirm/user prompts)
+UNINSTALL_COMMAND () {
+  sudo apt-get remove $@ -y
+}
+# Command to install a package and its dependencies (no confirm/user prompts)
+INSTALL_COMMAND () {
+  sudo apt-get install $@ -y
+}
+# Command to list all manually/explicitely installed packages
+LIST_COMMAND () {
+  apt-mark showmanual
+}
