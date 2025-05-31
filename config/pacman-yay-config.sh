75a7c66b04ea93338b369148c887686a82845b39
mantinhas <mantinhas@github.com>
2025-05-31 19:22:26 +0100

    workaround yay error code

--- config/pacman-yay-config.sh
+++ config/pacman-yay-config.sh
@@ -7,6 +7,9 @@ UNINSTALL_COMMAND () {
 # Command to install a package and its dependencies (no confirm/user prompts)
 INSTALL_COMMAND () {
   yay -S --noconfirm $@
+  # yay does not return error codes on some errors (package not found for example)
+  # This is a workaround return error code if the package was not installed
+  pacman -Qq $@ > /dev/null 2>&1 || return 1
 }
 # Command to list all manually/explicitely installed packages
 LIST_COMMAND () {
