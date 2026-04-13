524a22028d8931ed05ed39a16d81ad5887a83e8a
ostfriese4 <144824657+ostfriese4@users.noreply.github.com>
2026-04-13 20:40:51 +0200

    fixed pacman

--- config/pacman-config.sh
+++ config/pacman-config.sh
@@ -2,11 +2,13 @@ KEEPLISTFILE="/etc/declaro/packages.list"
 
 # Command to uninstall a package and its dependencies (no confirm/user prompts)
 UNINSTALL_COMMAND () {
-  sudo pacman -Rns --noconfirm $@
+  sudo pacman -D --asdeps $@
+  sudo pacman -Qdtq | sudo pacman -Rns --noconfirm -
 }
 # Command to install a package and its dependencies (no confirm/user prompts)
 INSTALL_COMMAND () {
-  sudo pacman -S --noconfirm $@
+  sudo pacman -S --noconfirm --needed $@
+  sudo pacman -D --asexplicit $@
 }
 # Command to list all manually/explicitely installed packages
 LIST_COMMAND () {
